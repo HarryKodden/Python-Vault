1dedb520983072289636504490c12072a5a31fc7
Harry Kodden <harry@e-tunity.nl>
2021-08-18 10:50:07 +0200

    Renamed nginx container to proxy, that is what it is !

--- app/app.py
+++ app/app.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     logging.debug('YES !')
-    return 'Hello super world!'
+    return 'Hello World!'
 
 if __name__ == '__main__':
     app.run(debug=(LOG_LEVEL == "DEBUG"))
